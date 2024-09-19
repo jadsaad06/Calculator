dbbddc58e9844693dadc56c058f503e80ba0dd96
Jad Saad <jadsaad06@gmail.com>
2024-09-19 01:06:13 -0700

    implemented delete button

--- script.js
+++ script.js
@@ -14,3 +14,7 @@ for(let i = 1; i <= 9; i++){
 const clear = document.querySelector("#clear");
 
 clear.addEventListener("click", () => userInput.textContent = '');
+
+const del = document.querySelector("#delete");
+
+del.addEventListener("click", () => userInput.textContent = userInput.textContent.substring(0, userInput.textContent.length - 1));
