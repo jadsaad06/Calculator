87d7189cb55a13a30c152b51db87fed6b233cd1a
Jad Saad <jadsaad06@gmail.com>
2024-09-19 01:12:53 -0700

    begin result calculation

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const numberContainer = document.querySelector('#numbers');
 const userInput = document.querySelector("#user-input");
 
+
 for(let i = 1; i <= 9; i++){
     const button = document.createElement("button");
     button.classList.add("btn");
     button.textContent = `${i}`;
     button.addEventListener("click", () =>{
         userInput.textContent += i;
+        result = parseInt(userInput.textContent)
     })    
     numberContainer.appendChild(button);
 }
@@ -18,3 +20,11 @@ clear.addEventListener("click", () => userInput.textContent = '');
 const del = document.querySelector("#delete");
 
 del.addEventListener("click", () => userInput.textContent = userInput.textContent.substring(0, userInput.textContent.length - 1));
+
+//calculations
+
+let result = 0;
+
+const divide = function(result){
+
+}
