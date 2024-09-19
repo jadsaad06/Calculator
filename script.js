ad28bf2dc867cc5b2478db4897d9632a7edc3d30
Jad Saad <jadsaad06@gmail.com>
2024-09-19 00:58:55 -0700

    Implemented the clear button

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const numberContainer = document.querySelector('#numbers');
+const userInput = document.querySelector("#user-input");
 
 for(let i = 1; i <= 9; i++){
     const button = document.createElement("button");
     button.classList.add("btn");
     button.textContent = `${i}`;
     button.addEventListener("click", () =>{
-        const userInput = document.querySelector("#user-input");
         userInput.textContent += i;
     })    
     numberContainer.appendChild(button);
 }
 
+const clear = document.querySelector("#clear");
+
+clear.addEventListener("click", () => userInput.textContent = '');
