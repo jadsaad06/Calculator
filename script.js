c58af82065ea9f2069cca02a8b915dc8b8a757a0
Jad Saad <jadsaad06@gmail.com>
2024-09-19 00:48:44 -0700

    Added event listeners to all the numbers and got the input to show up on calculator

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ for(let i = 1; i <= 9; i++){
     const button = document.createElement("button");
     button.classList.add("btn");
     button.textContent = `${i}`;
-    
+    button.addEventListener("click", () =>{
+        const userInput = document.querySelector("#user-input");
+        userInput.textContent += i;
+    })    
     numberContainer.appendChild(button);
 }
