65fc3e0d33e22ce36e4b5f8c53882c43fee18289
Jad Saad <jadsaad06@gmail.com>
2024-09-26 02:29:16 -0700

    Added an enum for the operations to allow for easy checking

--- script.js
+++ script.js
@@ -21,14 +21,43 @@ const del = document.querySelector("#delete");
 
 del.addEventListener("click", () => userInput.textContent = userInput.textContent.substring(0, userInput.textContent.length - 1));
 
-//calculations
+//operators
 
 const divide = document.querySelector("#divide");
 const multiply = document.querySelector("#multiply");
 const add = document.querySelector("#add");
-const subrract = document.querySelector("#subtract");
+const subtract = document.querySelector("#subtract");
+const equals = document.querySelector("#equals")
 
+const Operations = Object.freeze({
+    ADD: 1,
+    SUBTRACT: 2,
+    MULTIPLY: 3,
+    DIVIDE: 4
+});
 
-divide.addEventListener("click", function(){
+let operator = 0;
 
+divide.addEventListener("click", () => {
+    let num1 = userInput.textContent;
+    operator = 4;
+})
+
+multiply.addEventListener("click", () => {
+    let num1 = userInput.textContent;
+    operator = 3;
+})
+
+add.addEventListener("click", () => {
+    let num1 = userInput.textContent;
+    operator = 1;
+})
+
+subtract.addEventListener("click", () => {
+    let num1 = userInput.textContent;
+    operator = 2;
+})
+
+equals.addEventListener("click", () => {
+    
 })
