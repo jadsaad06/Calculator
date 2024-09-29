91776c83f290dc8ca7dd4291f6ca85fe165496a7
Jad Saad <jadsaad06@gmail.com>
2024-09-29 15:12:06 -0700

    Finished the Calculator:
    Clear button now clears all variables, equals now works correctly, new number is now inputted after pressing an operations button

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 const numberContainer = document.querySelector('#numbers');
 const userInput = document.querySelector("#user-input");
 
+let num1 = 0;
+let num2 = 0;
+
+let isCleared = false;
+
+const clear = document.querySelector("#clear");
+
+clear.addEventListener("click", function(){
+    isCleared = false;
+    operator = 0;
+    num1 = 0;
+    num2 = 0;
+    userInput.textContent = '';
+});
+
+const del = document.querySelector("#delete");
+
+del.addEventListener("click", () => userInput.textContent = userInput.textContent.substring(0, userInput.textContent.length - 1));
 
 for(let i = 1; i <= 9; i++){
     const button = document.createElement("button");
     button.classList.add("btn");
     button.textContent = `${i}`;
+    
+    
     button.addEventListener("click", () =>{
         userInput.textContent += i;
-        result = parseInt(userInput.textContent)
-    })    
+        
+        if(operator == 0){
+            num1 = parseInt(userInput.textContent)
+        }
+        else if(operator != 0 && isCleared == false) {
+            isCleared = true;
+            userInput.textContent = '';
+            userInput.textContent += i;
+            num2 = userInput.textContent;
+        }else{
+            num2 = userInput.textContent;
+        }
+        })    
+
     numberContainer.appendChild(button);
 }
-
-const clear = document.querySelector("#clear");
-
-clear.addEventListener("click", () => userInput.textContent = '');
-
-const del = document.querySelector("#delete");
-
-del.addEventListener("click", () => userInput.textContent = userInput.textContent.substring(0, userInput.textContent.length - 1));
 
 //operators
 
@@ -48,16 +72,36 @@ multiply.addEventListener("click", () => {
     operator = 3;
 })
 
-add.addEventListener("click", () => {
-    let num1 = userInput.textContent;
-    operator = 1;
-})
-
 subtract.addEventListener("click", () => {
     let num1 = userInput.textContent;
     operator = 2;
 })
 
+add.addEventListener("click", () => {
+    let num1 = userInput.textContent;
+    operator = 1;
+})
+
+
 equals.addEventListener("click", () => {
-    
+    if(isCleared){
+        switch(operator){
+            case 1:
+                num1 = num1 + num2;
+                break;
+            case 2:
+                num1 = num1 - num2;
+                break;
+            case 3:
+                num1 = num1 * num2;
+                break;
+            case 4:
+                if(num2 == 0) userInput.textContent = "Error";
+                else num1 = num1/num2;
+        }
+
+        userInput.textContent = num1;
+        isCleared = false;
+        num2 = 0;
+    }
 })
